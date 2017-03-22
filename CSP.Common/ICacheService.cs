3fff7be89b3d2936758063691d919cdb0c372874
Petar Petrov <primas.petrov@gmail.com>
2017-03-22 15:06:54 +0200

    Added interface

---
+++ CSP.Common/ICacheService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSP.Common
+{
+    public interface ICacheService
+    {
+        /// <summary>
+        /// Gets the value of the specified item name if exists or inserts it in the cache.
+        /// </summary>
+        /// <typeparam name="T">Any type</typeparam>
+        /// <param name="itemName">Name of the item.</param>
+        /// <param name="getDataFunc">The get data function.</param>
+        /// <param name="durationInSeconds">The duration in seconds.</param>
+        /// <returns>The cached item value</returns>
+        T Get<T>(string itemName, Func<T> getDataFunc, int durationInSeconds);
+
+        /// <summary>
+        /// Removes the specified item name.
+        /// </summary>
+        /// <param name="itemName">Name of the item.</param>
+        void Remove(string itemName);
+    }
+}
---
+++ CSP.ConsoleClient/Startup.cs
@@ -0,0 +1,10 @@
+﻿namespace CSP.ConsoleClient
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
